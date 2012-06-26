3e51dde47b720eefc19344e2646a6c300c14d943
Murtaza Husain <murtaza.husain@searce.com>
2012-06-26 19:33:19 +0530

    all

--- src/pm/client/templating.cljs
+++ src/pm/client/templating.cljs
@@ -1,7 +1,7 @@
 (ns pm.client.templating
   (:require [enfocus.core :as ef]
             [fetch.remotes :as remotes]
-            [cljsbinding :as binding])
+            [pm.client.utils :as utils])
   (:require-macros [enfocus.macros :as em]
                    [fetch.macros :as fm])
   (:use-macros [enfocus.macros :only [defsnippet deftemplate defaction]]))
@@ -11,9 +11,10 @@
 (comment (defn get-html [url sel]
   (fm/letrem [html-templ (get-template url sel)] html-templ)))
 
-(defaction layout [c]
+(defn layout [c]
   "Takes html string as input and inserts it into the DOM" 
-  [".cljs-main"] (em/content c))
+  (em/at (doc)
+         [".cljs-main"] (em/content c)))
 
 (defsnippet toolbar "templates/components.html" ["#top-navbar"] []
   ["#hello"] (em/content "hi"))
@@ -22,10 +23,15 @@
   ["title"] (em/content title))
 
 (defaction set-toolbar [] 
-  ["#header"] (em/content (toolbar)))
+  ["#header"] (em/content (toolbar))
+  ["#add-new-project"] (em/listen :click add-project))
 
-(defn ^:export hello []
-  (js/alert "hi from cljsbind"))
+(defsnippet project-page "templates/components.html" ["#project-add-section"] []
+  ["#my-modal"] (em/content "Hello Vidit"))
 
-                                      ; ["#add-new-project"] (em/listen :click #(js/alert "hi"))
+(defn add-project []
+  (let [c (project-page)]
+    (layout c)))
 
+(comment
+)
