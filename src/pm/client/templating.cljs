7bad0b2bdc9e8346464cd4bcc144ecc464289e4a
Murtaza Husain <murtaza.husain@searce.com>
2012-06-29 07:27:06 +0530

    project-add bindings

--- src/pm/client/templating.cljs
+++ src/pm/client/templating.cljs
@@ -1,7 +1,9 @@
 (ns pm.client.templating
   (:require [enfocus.core :as ef]
             [fetch.remotes :as remotes]
-            [pm.client.utils :as utils])
+            [pm.client.utils :as utils]
+            [jayq.core :as jq]
+            [cljsbinding :as binding])
   (:require-macros [enfocus.macros :as em]
                    [fetch.macros :as fm])
   (:use-macros [enfocus.macros :only [defsnippet deftemplate defaction]]))
@@ -11,18 +13,20 @@
 (comment (defn get-html [url sel]
   (fm/letrem [html-templ (get-template url sel)] html-templ)))
 
+;"Takes html string as input and inserts it into the DOM"
+(defaction layout
+  [c]
+  [".cljs-main"] (em/content c))
+
 ;;This Works
-(defn layout [c]
-  "Takes html string as input and inserts it into the DOM" 
+
+(comment
+  (defn layout
+  "Takes html string as input and inserts it into the DOM"
+  [c]
   (em/at (doc)
          [".cljs-main"] (em/content c)))
-
-;;This does not work
-(comment
-  (defaction layout [c]
-  "Takes html string as input and inserts it into the DOM" 
-  [".cljs-main"] (em/content c))
-  )
+ )
 
 (defsnippet toolbar "templates/components.html" ["#top-navbar"] []
   ["#hello"] (em/content "hi"))
@@ -34,12 +38,14 @@
   ["#header"] (em/content (toolbar))
   ["#add-new-project"] (em/listen :click add-project))
 
-(defsnippet project-page "templates/components.html" ["#project-add-section"] []
-  ["#my-modal"] (em/content "Hello Vidit"))
+(defsnippet project-page "templates/components.html" ["#project-add-section"] [])
 
 (defn add-project []
   (let [c (project-page)]
-    (layout c)))
+    (layout c))
+  (.modal (jq/$ "#project-add-modal") "show")
+  (binding/bindall (jq/$ "#project-add-modal")))
 
-(comment
-)
+(defn meclicked
+  []
+  (js/alert "cljs-binding clicked"))
